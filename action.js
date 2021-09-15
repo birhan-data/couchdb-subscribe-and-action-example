e525bc76952586d66db1682fc8ccaebfa2a723b3
esurface <esurface@rti.org>
2021-09-15 15:59:38 -0400

    Add logic to skip deleted forms

--- action.js
+++ action.js
@@ -1,5 +1,15 @@
 module.exports = async function(change, db) {
-  const doc = await db.get(change.id)
+  var doc = undefined
+  try {
+    doc = await db.get(change.id)
+  } catch (err) {
+    if (err.error == 'not_found') {
+      return 0
+    } else {
+      console.log(`Error getting doc ${doc._id}: ${err.error}`)
+      return -1
+    }
+  }
   if (!doc.dataVersion || doc.dataVersion < 1) {
     doc.foo = true
     doc.dataVersion = 1
